5ebbc6aee4ef82635b787b2fac1de6caab6533cd
bennibloo <57432484+bennibloo@users.noreply.github.com>
2021-04-13 00:00:24 -0400

    DB server interaction with ATM (in main.c)

--- main.c
+++ main.c
@@ -2,44 +2,60 @@
 #include<string.h>
 #include<stdlib.h>
 #include<mqueue.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "extra_file.h"
+#include "DBserver.h"
 
 
-/* ========================== QUEUE PREREQUISITES ========================== */
-#define ACCOUNT_NUMBER_SIZE 5
-#define PIN_NUMBER_SIZE 3
-#define DB_RESPONSE_SIZE 8
-#define MAX_QUEUE_MSG_SIZE 16
+/**
+ * Function that gets user input in the form of an integer. Returns -1 if there is an error, otherwise returns an integer.
+ * Special case: if user inputs x, returns -2.
+ *
+ * paramater is a char array which will be displayed to the user.
+ */
+int getUserInput(char *message){
+    char buffer[10];
+    int result;
 
+    puts(message);
 
-/* ========================== STRUCT PREREQUISITES ========================== */
-/*
- * The PIN and account number struct to be transmitted to the
- * DB upon submission from the client.
- * */
-typedef struct SEND_MSG_STRUCT{
-    char PIN_NUMBER[3];         // The client's PIN
-    char ACCOUNT_NUMBER[5];     // The client's account number
-} SEND_MSG_STRUCT;
-static mqd_t SEND_DESTINATION = -1;
-static mqd_t RCV_DESTINATION = -1;
+    fgets(buffer,10,stdin);
 
-/*
- * The banking request struct to be transmitted to the
- * DB when the user selects their needed service
- * */
-typedef struct BANKING_MSG_STRUCT{
-    char BALANCE_REQUEST[15];
-    char WITHDRAW_REQUEST[16];     // Only the requested banking service will have a corresponding value of 1
-    char WITHDRAW_AMOUNT;                      // For a withdrawal service; the user-provided withdrawal amount is a float
-} BANKING_MSG_STRUCT;
+    if(strncmp(buffer,"x",1) == 0){
+        return -2;
+    }else if((result = atoi(buffer) == -1)){
+        return -1;
+    }
 
-/*
- * The response struct returned by the database when it is
- * sent one of the above MSG_STRUCT's
- * */
-typedef struct RCV_MSG_STRUCT{
-    char RESPONSE[DB_RESPONSE_SIZE];                       // The response must be no more than 9 chars
-}RCV_MSG_STRUCT;
+    return result;
+
+};
+
+/**
+ * Function that gets user input in the form of an float. Returns -1 if there is an error, otherwise returns an integer.
+ * Special case: if user inputs x, returns -2.
+ *
+ * paramater is a char array which will be displayed to the user.
+ */
+float getUserInputFloat(char *message){
+    char buffer[10];
+    int result;
+
+    puts(message);
+
+    fgets(buffer,10,stdin);
+
+    if(strncmp(buffer,"x",1) == 0){
+        return -2;
+    }else if((result = atof(buffer) == -1)){
+        return -1;
+    }
+
+    return result;
+
+};
 
 
 /* ========================== RESOURCE PREREQUISITES & OTHER ========================== */
@@ -57,95 +73,113 @@ int EQUALITY_CHECK(char *ARR1, char *ARR2, int LENGTH){
     return 1;                               // They are equal; return 1
 }
 
+/*
+ * Method to start the ATM system
+ * */
 void ATM_START() {
 
-    int ATTEMPTS_REMAINING = 3;         // How many attempts the user has left
-    int status;                         // Represents the status when sending and receiving on the message queue
-    char ACCOUNT_NUM[5];                // User given account number
-    char PIN[3];                        // User given PIN
+    GenericMessage sendingMessage;
+    GenericMessage receivingMessage;
 
-    SEND_MSG_STRUCT SEND_MSG;            // Sends PIN and account number to DB
-    RCV_MSG_STRUCT RCV_MSG;             // Receives the response from the DB
+    Mailbox *mailbox;
+
+    int msgID = getmsgQueueID();     //
+    int attempts = 3;               // How many attempts the user has left
+    int status;                     // Represents the status when sending and receiving on the message queue
+
+    int accountNumber = -1;          // User given account number
+    int PIN = 1;                    // User given PIN
+    int balance = -1.0;
+
+    char response;
+
+    sendingMessage.message_type = clientMessageType;
+    receivingMessage.message_type = serverMessageType;
+
+    resetDataBundle(&sendingMessage.data);
+    resetDataBundle(&receivingMessage.data);
+
+
 
     printf("<=====================> ATM <=====================>");
 
+    /* Keeping the ATM running */
     do {
-        printf("Kindly enter your account number:- ");
-        scanf("%c", ACCOUNT_NUM);
-        printf("\nKindly enter you PIN:- ");
-        scanf("%c", PIN);
+        /* Getting the account number and account number from the user*/
+        printf("Kindly enter your account number:- %d\n", accountNumber);
+        printf("Kindly enter you PIN:- %d\n", PIN);
 
-        for (int i = 0; i < 5; ++i) {                       // Making the account number in the struct equal to the user-given account number
-            SEND_MSG.ACCOUNT_NUMBER[i] = ACCOUNT_NUM[i];
-        }
-        for (int i = 0; i < 3; ++i) {                       // Making the PIN in the struct equal to the user-given PIN
-            SEND_MSG.PIN_NUMBER[i] = PIN[i];
-        }
+        /* Making the account number in the struct equal to the user-given account number */
+        sendingMessage.data.account.accountNumber = accountNumber;
 
-        status = mq_send(SEND_DESTINATION, (const char*) &ACCOUNT_NUM, sizeof(ACCOUNT_NUM),
-                         1);    // Sending the SEND struct; contains PIN and account number
-        if (status < 0) {               // If the returned status is -1 there is an error
-            printf("PIN message cannot send");
+        /* Making the PIN in the struct equal to the user-given PIN */
+        sendingMessage.data.account.pin = PIN;
+
+        status = sendMessage(msgID, sendingMessage, NOBLOCK);           // Sending the SEND struct; contains PIN and account number
+
+        /* If the returned status is -1 there is an error in sending the message to the DB*/
+        if (status < 0) {
+            printf("Message cannot send");
             exit(EXIT_FAILURE);
         }
 
-        status = mq_receive(RCV_DESTINATION, (char *) &RCV_MSG, sizeof(RCV_MSG), 0);       // Receiving the DB struct; contains response
-        if (status < 0) {           // If the returned status is -1 there is an error
-            printf("DB message cannot be received");
+        status = receiveMessage(msgID, &receivingMessage, NOBLOCK);     // Receiving the DB struct; contains response
+
+        /* If the returned status is -1 there is an error in receiving the message from the DB */
+        if (status < 0) {
+            printf("Message cannot be received");
             exit(EXIT_FAILURE);
         }
 
-        // If we receive the "OK" from the DB we may proceed to selection
-        if (EQUALITY_CHECK(RCV_MSG.RESPONSE, "OK", 4)) {
-            char SELECTION[8];                  // Ther user-given operation selection
-            BANKING_MSG_STRUCT BANKING_MSG;     // Struct containing the information regarding the selection ("BALANCE", "WITHDRAW" and the amount).
+        response = receivingMessage.data.response;
+        /* If we receive the "OK" from the DB we may proceed to selection */
+        if (EQUALITY_CHECK(&response, "OK", 2)) {
+            char selection[8];                                                                          // The selection (can either be "BALANCE", or "WITHDRAW")
 
-            printf("Kindly type your service:- \"BALANCE\" or \"WITHDRAW\"\n");
+            /* If the user wishes to have multiple requests we can do this with a loop */
             do {
-                scanf("%c", SELECTION);
+                printf("Kindly enter your service; \"BALANCE\" or \"WITHDRAW\":- %s\n", selection);
 
-                // If its a BALANCE request
-                if (EQUALITY_CHECK(SELECTION, "BALANCE", 7)) {
-                    memcpy(BANKING_MSG.BALANCE_REQUEST, "BALANCE_REQUEST", 15);                         // Copies the request into the BALANCE_REQUEST field
-                    mq_send(SEND_DESTINATION, (const char*) &BANKING_MSG, sizeof(BANKING_MSG), 1);      // Sends the BALANCE request to the DB
-                    mq_receive(SEND_DESTINATION, (char*) &BANKING_MSG, sizeof(BANKING_MSG), 0);         // Receives the response from the DB
-                    printf("\nCurrent balance:- $%s", BANKING_MSG.BALANCE_REQUEST);
+                /* If its a BALANCE request */
+                if (EQUALITY_CHECK(selection, "BALANCE", 7)) {
+                    sendingMessage.message_type = BALANCE;
+                    sendMessage(msgID, sendingMessage, NOBLOCK);
+                    receiveMessage(msgID, &receivingMessage, NOBLOCK);
+                    printf("\nCurrent balance:- $%f", receivingMessage.data.account.funds);
                     break;
                 }
 
-                // If its a WITHDRAW request
-                else if (EQUALITY_CHECK(SELECTION, "WITHDRAW", 8)) {
-                    double AMOUNT;                                                                              // The amount to withdraw
+                /* If its a WITHDRAW request */
+                else if (EQUALITY_CHECK(selection, "WITHDRAW", 8)) {
+                    double AMOUNT;                                                                      // The amount to withdraw
+                    sendingMessage.message_type = WITHDRAW;
                     printf("\nKindly enter the amount to withdraw (CAD assumed):- ");
                     scanf("%lf", &AMOUNT);
-                    memcpy(BANKING_MSG.WITHDRAW_REQUEST, "WITHDRAW_REQUEST", 16);                       // Copies the request into the WITHDRAW_REQUEST
-                    memcpy(BANKING_MSG.WITHDRAW_AMOUNT, &AMOUNT, 16);                                       // Copies the amount in the WITHDRAW_AMOUNT
-                    mq_send(SEND_DESTINATION, (const char*) &BANKING_MSG, sizeof(BANKING_MSG), 1);      // Sends the WITHDRAW request to the DB
-                    mq_receive(SEND_DESTINATION, (char*) &BANKING_MSG, sizeof(BANKING_MSG), 0);         // Receives the response from the DB
+                    sendMessage(msgID, sendingMessage, NOBLOCK);
+                    receiveMessage(msgID, &receivingMessage, NOBLOCK);
+                    printf("\nCurrent balance:- $%f", receivingMessage.data.account.funds);       // Receives the response from the DB
 
-                    // If there is insufficient funds in the account
-                    if (EQUALITY_CHECK(RCV_MSG.RESPONSE, "NSF", 3)) {
+                    response = receivingMessage.data.response;
+                    /* If there is insufficient funds in the account */
+                    if (EQUALITY_CHECK(&response, "NSF", 3)) {
                         printf("\nInsufficient funds\n");
                     }
-
-                    // If there is sufficient funds in the account
-                    else if (EQUALITY_CHECK(RCV_MSG.RESPONSE, "FUNDS_OK", 8)) {
-                        printf("\nCurrent balance:- $%f",
-                               BANKING_MSG.BALANCE_REQUEST);        // Print balance - withdrawal
+                    /* If there is sufficient funds in the account */
+                    else if (EQUALITY_CHECK(&response, "FUNDS_OK", 8)) {
+                        printf("\nCurrent balance:- $%f", receivingMessage.data.account.funds);        // Print balance minus withdrawal
                     }
                     break;
                 }
-
             } while (0);
         }
 
-        // If the user inputs the wrong accont or PIN; dock them an attempt
+        /* If the user inputs the wrong account or PIN; dock them an attempt */
         else {
             printf("\nIncorrect account or PIN\n");
-            ATTEMPTS_REMAINING -= 1;
+            attempts -= 1;
 
-            // If they are out of attempts; exit (what we chose to do)
-            if (ATTEMPTS_REMAINING <= 0){
+            /* If they are out of attempts; exit (what we chose to do) */
+            if (attempts <= 0){
                 printf("\nAccount is locked\n");
                 exit(EXIT_FAILURE);                         // Not specified if should exit
             }
@@ -156,6 +190,7 @@ void ATM_START() {
 
 int main() {
     ATM_START();
+
 }
 
 
@@ -167,10 +202,4 @@ int main() {
  * 4. https://www.thegeekstuff.com/2012/05/c-mutex-examples/
  * 5. https://gist.github.com/gustavorv86/9e98621b44222114524399a3b4302ddb
  * 6. https://pubs.opengroup.org/onlinepubs/009695399/basedefs/mqueue.h.html
- * 7.
- * */
-
-/*
- * NOTES:-
- * - Still need a message queue, not sure how to implement
  * */
